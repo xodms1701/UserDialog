6cec934ab6d2c12a58ef465a858386b217bfcfb4
xodms1701 <kimrrrrt123@naver.com>
2017-05-29 20:40:07 +0900

    #2 done

--- app/src/main/java/org/techtown/userdialog/MainActivity.java
+++ app/src/main/java/org/techtown/userdialog/MainActivity.java
@@ -1,13 +1,33 @@
 package org.techtown.userdialog;
 
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+import static org.techtown.userdialog.R.layout.dialog;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button but = (Button)findViewById(R.id.but);
+        but.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        View dlgV = View.inflate(getApplicationContext(), R.layout.dialog, null);
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Input User Information");
+        dialog.setIcon(R.mipmap.ic_launcher);
+        dialog.setView(dlgV);
+        dialog.setPositiveButton("OK",null);
+        dialog.setNegativeButton("CANCEL",null);
+        dialog.show();
     }
 }
